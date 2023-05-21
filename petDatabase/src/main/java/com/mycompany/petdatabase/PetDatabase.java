861769a4e03c954d3fb700ff2a9a171636729418
Dylan Johnson <djstealadunk+github@gmail.com>
2023-05-21 22:18:16 +0100

    Release 1

--- petDatabase/src/main/java/com/mycompany/petdatabase/PetDatabase.java
+++ petDatabase/src/main/java/com/mycompany/petdatabase/PetDatabase.java
@@ -49,8 +49,8 @@ public class PetDatabase {//Was this supposed to be called PetDatabase? I would 
 		System.out.println("\n\nWhat would you like to do?");
 		System.out.println("1) View all pets");
 		System.out.println("2) Add more pets");
-		System.out.println("3) Update an existing pet");
-		System.out.println("4) Remove an existing pet");
+//		System.out.println("3) Update an existing pet");
+//		System.out.println("4) Remove an existing pet");
 //		System.out.println("5) Search pets by name");
 //		System.out.println("6) Search pets by age");
 		System.out.println("7) Exit program\n");
