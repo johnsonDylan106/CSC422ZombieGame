5639e1b0a5ca67d17756b091558a6fa70af7b0ce
Dylan Johnson <djstealadunk+github@gmail.com>
2023-05-21 22:39:32 +0100

    Release 2

--- petDatabase/src/main/java/com/mycompany/petdatabase/PetDatabase.java
+++ petDatabase/src/main/java/com/mycompany/petdatabase/PetDatabase.java
@@ -49,7 +49,8 @@ public class PetDatabase {
 		System.out.println("2) Add more pets");
 //		System.out.println("3) Update an existing pet");
 //		System.out.println("4) Remove an existing pet");
-s
+		System.out.println("5) Search pets by name");
+		System.out.println("6) Search pets by age");
 		System.out.println("7) Exit program\n");
 		System.out.print("Your choice: ");
 		int choice = input.nextInt();
