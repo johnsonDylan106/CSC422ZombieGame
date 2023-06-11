2e9c49cfb1ad0377820b43d8491fa7ca300160d5
Dylan Johnson <djstealadunk+githuh@gmail.com>
2023-06-11 17:40:37 -0500

    Zombie Project

---
+++ ZombieProject/src/main/java/com/mycompany/zombieproject/Survivor.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc422assn5;
+
+/**
+ *
+ * @author veltk
+ */
+public class Survivor {
+    
+    public String name;
+    public int attack;
+    public int health;
+    
+    Survivor(String name, int attack, int health) {
+        this.name = name;
+        this.attack = attack;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    
+}
---
+++ ZombieProject/src/main/java/com/mycompany/zombieproject/ZombieProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zombieproject;
+
+/**
+ *
+ * @author dylan
+ */
+public class ZombieProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
